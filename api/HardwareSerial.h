54cad0a749836cd1e6edaeeeca4ad410fabc1848
Jeffrey Kuhn <drjrkuhn@gmail.com>
2022-03-14 21:48:59 -0400

    reset api

--- api/HardwareSerial.h
+++ api/HardwareSerial.h
@@ -14,10 +14,6 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-  Modified 28 September 2010 by Mark Sproul
-  Modified 14 August 2012 by Alarus
-  Modified 3 December 2013 by Matthijs Kooijman
 */
 
 #pragma once
@@ -90,70 +86,20 @@ namespace arduino {
 
 class HardwareSerial : public Stream
 {
-  protected:
-    volatile uint8_t * const _ubrrh;
-    volatile uint8_t * const _ubrrl;
-    volatile uint8_t * const _ucsra;
-    volatile uint8_t * const _ucsrb;
-    volatile uint8_t * const _ucsrc;
-    volatile uint8_t * const _udr;
-    // Has any byte been written to the UART since begin()
-    bool _written;
-
-    volatile rx_buffer_index_t _rx_buffer_head;
-    volatile rx_buffer_index_t _rx_buffer_tail;
-    volatile tx_buffer_index_t _tx_buffer_head;
-    volatile tx_buffer_index_t _tx_buffer_tail;
-
-    // Don't put any members after these buffers, since only the first
-    // 32 bytes of this struct can be accessed quickly using the ldd
-    // instruction.
-    unsigned char _rx_buffer[SERIAL_RX_BUFFER_SIZE];
-    unsigned char _tx_buffer[SERIAL_TX_BUFFER_SIZE];
-
   public:
-    inline HardwareSerial(
-      volatile uint8_t *ubrrh, volatile uint8_t *ubrrl,
-      volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
-      volatile uint8_t *ucsrc, volatile uint8_t *udr);
-    void begin(unsigned long baud) { begin(baud, SERIAL_8N1); }
-    void begin(unsigned long, uint8_t);
-    void end();
-    virtual int available(void);
-    virtual int peek(void);
-    virtual int read(void);
-    virtual int availableForWrite(void);
-    virtual void flush(void);
-    virtual size_t write(uint8_t);
-    inline size_t write(unsigned long n) { return write((uint8_t)n); }
-    inline size_t write(long n) { return write((uint8_t)n); }
-    inline size_t write(unsigned int n) { return write((uint8_t)n); }
-    inline size_t write(int n) { return write((uint8_t)n); }
+    virtual void begin(unsigned long) = 0;
+    virtual void begin(unsigned long baudrate, uint16_t config) = 0;
+    virtual void end() = 0;
+    virtual int available(void) = 0;
+    virtual int peek(void) = 0;
+    virtual int read(void) = 0;
+    virtual void flush(void) = 0;
+    virtual size_t write(uint8_t) = 0;
     using Print::write; // pull in write(str) and write(buf, size) from Print
-    operator bool() { return true; }
-
-    // Interrupt handlers - Not intended to be called externally
-    inline void _rx_complete_irq(void);
-    void _tx_udr_empty_irq(void);
+    virtual operator bool() = 0;
 };
 
-#if defined(UBRRH) || defined(UBRR0H)
-  extern HardwareSerial Serial;
-  #define HAVE_HWSERIAL0
-#endif
-#if defined(UBRR1H)
-  extern HardwareSerial Serial1;
-  #define HAVE_HWSERIAL1
-#endif
-#if defined(UBRR2H)
-  extern HardwareSerial Serial2;
-  #define HAVE_HWSERIAL2
-#endif
-#if defined(UBRR3H)
-  extern HardwareSerial Serial3;
-  #define HAVE_HWSERIAL3
-#endif
-
+// XXX: Are we keeping the serialEvent API?
 extern void serialEventRun(void) __attribute__((weak));
 
 }
--- api/Print.h
+++ api/Print.h
@@ -20,12 +20,9 @@
 
 #include <inttypes.h>
 #include <stdio.h> // for size_t
-#include <string> // for char_traits
 
 #include "String.h"
 #include "Printable.h"
-
-#include <ostream>
 
 #define DEC 10
 #define HEX 16
@@ -33,302 +30,67 @@
 #define BIN 2
 
 namespace arduino {
-	template <typename charT, typename traits = std::char_traits<charT> >
-	class basic_Print : basic_ostream<charT, traits>
-	{
-	protected:
-		void setWriteError(int err = 1) { write_error_ = err; }
-	public:
-		basic_print((basic_streambuf<charT, traits>* sbuf)
-			: basic_ostream(sbuf), write_error_(0) {
-		}
 
-		int getWriteError() { return write_error_; }
-		void clearWriteError() { setWriteError(0); }
+class Print
+{
+  private:
+    int write_error;
+    size_t printNumber(unsigned long, uint8_t);
+    size_t printULLNumber(unsigned long long, uint8_t);
+    size_t printFloat(double, int);
+  protected:
+    void setWriteError(int err = 1) { write_error = err; }
+  public:
+    Print() : write_error(0) {}
 
-		virtual size_t write(uint8_t) = 0;
+    int getWriteError() { return write_error; }
+    void clearWriteError() { setWriteError(0); }
 
-		size_t write(const char* str) {
-			if (str == NULL) return 0;
-			return write((const uint8_t*)str, strlen(str));
-		}
-		virtual size_t write(const uint8_t* buffer, size_t size) {
-			size_t n = 0;
-			while (size--) {
-				if (write(*buffer++)) n++;
-				else break;
-			}
-			return n;
-		}
-		size_t write(const char* buffer, size_t size) {
-			return write((const uint8_t*)buffer, size);
-		}
+    virtual size_t write(uint8_t) = 0;
+    size_t write(const char *str) {
+      if (str == NULL) return 0;
+      return write((const uint8_t *)str, strlen(str));
+    }
+    virtual size_t write(const uint8_t *buffer, size_t size);
+    size_t write(const char *buffer, size_t size) {
+      return write((const uint8_t *)buffer, size);
+    }
 
-		// default to zero, meaning "a single write may block"
-		// should be overridden by subclasses with buffering
-		virtual int availableForWrite() { return 0; }
+    // default to zero, meaning "a single write may block"
+    // should be overridden by subclasses with buffering
+    virtual int availableForWrite() { return 0; }
 
-		size_t print(const __FlashStringHelper* ifsh) {
-			return print(reinterpret_cast<const char*>(ifsh));
-		}
+    size_t print(const __FlashStringHelper *);
+    size_t print(const String &);
+    size_t print(const char[]);
+    size_t print(char);
+    size_t print(unsigned char, int = DEC);
+    size_t print(int, int = DEC);
+    size_t print(unsigned int, int = DEC);
+    size_t print(long, int = DEC);
+    size_t print(unsigned long, int = DEC);
+    size_t print(long long, int = DEC);
+    size_t print(unsigned long long, int = DEC);
+    size_t print(double, int = 2);
+    size_t print(const Printable&);
 
-		size_t print(const String& s)
-		{
-			return write(s.c_str(), s.length());
-		}
-		size_t print(const char[] str)
-		{
-			return write(str);
-		}
-		size_t print(char c)
-		{
-			return write(c);
-		}
-		size_t print(unsigned char b, int base = DEC)
-		{
-			return print((unsigned long)b, base);
-		}
-		size_t print(int n, int base = DEC)
-		{
-			return print((long)n, base);
-		}
-		size_t print(unsigned int n, int base = DEC)
-		{
-			return print((unsigned long)n, base);
-		}
-		size_t print(long n, int base = DEC)
-		{
-			if (base == 0) {
-				return write(n);
-			}
-			else if (base == 10) {
-				if (n < 0) {
-					int t = print('-');
-					n = -n;
-					return printNumber(n, 10) + t;
-				}
-				return printNumber(n, 10);
-			}
-			else {
-				return printNumber(n, base);
-			}
-		}
-		size_t print(unsigned long n, int base = DEC)
-		{
-			if (base == 0) return write(n);
-			else return printNumber(n, base);
-		}
-		size_t print(long long n, int base = DEC)
-		{
-			if (base == 0) {
-				return write(n);
-			}
-			else if (base == 10) {
-				if (n < 0) {
-					int t = print('-');
-					n = -n;
-					return printULLNumber(n, 10) + t;
-				}
-				return printULLNumber(n, 10);
-			}
-			else {
-				return printULLNumber(n, base);
-			}
-		}
-		size_t print(unsigned long long n, int base = DEC)
-		{
-			if (base == 0) return write(n);
-			else return printULLNumber(n, base);
-		}
-		size_t print(double, int = 2)
-		{
-			return printFloat(n, digits);
-		}
-		size_t print(const Printable& x)
-		{
-			return x.printTo(*this);
-		}
+    size_t println(const __FlashStringHelper *);
+    size_t println(const String &s);
+    size_t println(const char[]);
+    size_t println(char);
+    size_t println(unsigned char, int = DEC);
+    size_t println(int, int = DEC);
+    size_t println(unsigned int, int = DEC);
+    size_t println(long, int = DEC);
+    size_t println(unsigned long, int = DEC);
+    size_t println(long long, int = DEC);
+    size_t println(unsigned long long, int = DEC);
+    size_t println(double, int = 2);
+    size_t println(const Printable&);
+    size_t println(void);
 
-		size_t println(const __FlashStringHelper* ifsh)
-		{
-			size_t n = print(ifsh);
-			n += println();
-			return n;
-		}
-		size_t println(const String& s)
-		{
-			size_t n = print(s);
-			n += println();
-			return n;
-		}
-		size_t println(const char[] c)
-		{
-			size_t n = print(c);
-			n += println();
-			return n;
-		}
-		size_t println(char)
-		{
-			size_t n = print(c);
-			n += println();
-			return n;
-		}
-		size_t println(unsigned char b, int base = DEC)
-		{
-			size_t n = print(b, base);
-			n += println();
-			return n;
-		}
-		size_t println(int, int = DEC)
-		{
-			size_t n = print(num, base);
-			n += println();
-			return n;
-		}
-		size_t println(unsigned int, int = DEC)
-		{
-			size_t n = print(num, base);
-			n += println();
-			return n;
-		}
-		size_t println(long, int = DEC)
-		{
-			size_t n = print(num, base);
-			n += println();
-			return n;
-		}
-		size_t println(unsigned long, int = DEC)
-		{
-			size_t n = print(num, base);
-			n += println();
-			return n;
-		}
-		size_t println(long long, int = DEC)
-		{
-			size_t n = print(num, base);
-			n += println();
-			return n;
-		}
-		size_t println(unsigned long long, int = DEC)
-		{
-			size_t n = print(num, base);
-			n += println();
-			return n;
-		}
-		size_t println(double, int = 2)
-		{
-			size_t n = print(num, digits);
-			n += println();
-			return n;
-		}
-		size_t println(const Printable&)
-		{
-			size_t n = print(x);
-			n += println();
-			return n;
-		}
-		size_t println(void)
-		{
-			return write("\r\n");
-		}
-
-		virtual void flush() { /* Empty implementation for backward compatibility */ }
-	private:
-		int write_error_;
-
-		size_t printNumber(unsigned long n, uint8_t base)
-		{
-			traits.offtype bufsize = std::numeric_limits<unsigned long>.digits();
-			charT buf[bufsize + 1]; // Assumes 8-bit chars plus zero byte.
-			charT* end = &buf[bufsize - 1];
-
-			*end = '\0';
-
-			// prevent crash if called with base == 1
-			if (base < 2) base = 10;
-
-			do {
-				charT c = n % base;
-				n /= base;
-
-				*--end = c < 10 ? c + '0' : c + 'A' - 10;
-			} while (n);
-
-			return print(end);
-		}
-
-		size_t printULLNumber(unsigned long long, uint8_t)
-		{
-			traits.offtype bufsize = std::numeric_limits<unsigned long long>.digits();
-			charT buf[bufsize + 1]; // Assumes 8-bit chars plus zero byte.
-			charT* end = &buf[bufsize - 1];
-
-			*end = '\0';
-
-			// prevent crash if called with base == 1
-			if (base < 2) base = 10;
-
-			do {
-				charT c = n % base;
-				n /= base;
-
-				*--end = c < 10 ? c + '0' : c + 'A' - 10;
-			} while (n);
-
-			return print(end);
-		}
-
-		size_t printFloat(double, int)
-		{
-			if (digits < 0)
-				digits = 2;
-
-			size_t n = 0;
-
-			if (isnan(number)) return print("nan");
-			if (isinf(number)) return print("inf");
-			if (number > 4294967040.0) return print("ovf");  // constant determined empirically
-			if (number < -4294967040.0) return print("ovf");  // constant determined empirically
-
-															 // Handle negative numbers
-			if (number < 0.0)
-			{
-				n += print('-');
-				number = -number;
-			}
-
-			// Round correctly so that print(1.999, 2) prints as "2.00"
-			double rounding = 0.5;
-			for (uint8_t i = 0; i < digits; ++i)
-				rounding /= 10.0;
-
-			number += rounding;
-
-			// Extract the integer part of the number and print it
-			unsigned long int_part = (unsigned long)number;
-			double remainder = number - (double)int_part;
-			n += print(int_part);
-
-			// Print the decimal point, but only if there are digits beyond
-			if (digits > 0) {
-				n += print(".");
-			}
-
-			// Extract digits from the remainder one at a time
-			while (digits-- > 0)
-			{
-				remainder *= 10.0;
-				unsigned int toPrint = (unsigned int)remainder;
-				n += print(toPrint);
-				remainder -= toPrint;
-			}
-
-			return n;
-		}
-
-	};
-
-	typedef basic_Print<char> Print;
+    virtual void flush() { /* Empty implementation for backward compatibility */ }
+};
 
 }
 using arduino::Print;
